b8f40b617113d10479f1eb63e1e4df589b111c0b
Glitch (hello-webpage) <none>
2020-07-03 08:36:33 +0000

    🚏🚣 Checkpoint ./script.js:18290274/1879

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
 var roots = [];
 function regulaFalsi(f = function(x,a = 1){return  x*x - 2*x + 4}, a, b, dx = 10E-9){
-    if (f(xmin) * f(xmax) >= 0) { 
-        ret
-    } 
-    let c = xmin;    
-    for (let i=0; i < 1000; i++) { 
-        c = (xmin*f(xmax) - xmax*f(xmin))/ (f(xmax) - f(xmin)); 
-        if (f(c)==0) 
-            break; 
-        else if (f(c)*f(xmin) < 0) 
-            xmax = c; 
-        else
-            xmin = c; 
-    } 
-    roots.push(c);
+   let root,
+       fr,
+       i,
+       side=0,
+       fa = f(a),
+       fb = f(b);
+   for (i = 0; i < 1000; i++){
+       root = (fa*b - fb*a) / (fa - fb);
+       if (Math.abs(b-a) < dx*Math.abs(b+a)) break;
+       fr = f(root);
+       if (fr * fb > 0)
+       {
+         b = root; fb = fr;
+         if (side==-1) fa /= 2;
+         side = -1;
+       }
+       else if (fa * fr > 0)
+       {
+         a = root;  fa = fr;
+         if (side==+1) fb /= 2;
+         side = +1;
+       }
+       else
+       {
+         break;
+       } 
+    }
+    roots.push(root);
 }
-for(let i = xmin; i < xmax; i += )
-console.log(regulaFalsi());
+var xmin = -5, xmax = 5;
+for(let i = xmin; i <= xmax; i += 1/Math.abs(xmax - xmin)){
+  regulaFalsi(function(x,a = 1){return  x*x - 2*x + 4}, i, xmax, 10E-9)
+}
+console.log(roots);
