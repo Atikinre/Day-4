fd2b178527fdf2d4ec569991b731876b38535c05
Glitch (hello-webpage) <none>
2020-07-03 05:58:53 +0000

    😻🛋 Checkpoint ./script.js:18290274/553 ./index.html:18290274/402

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-https://cdnjs.cloudflare.com/ajax/libs/mathjs/7.0.2/math.min.js
-console.log("hi");
+var math = require('https://cdnjs.cloudflare.com/ajax/libs/mathjs/7.0.2/math.js');
+console.log(math.evaluate(math.sin(Math.PI)));
