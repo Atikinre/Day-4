d9e1d706105db57c61ce89d5a9e3871c50f2240b
Glitch (hello-webpage) <none>
2020-07-03 08:47:35 +0000

    ⛳️🍳 Checkpoint ./script.js:18290274/2385

--- script.js
+++ script.js
@@ -1,36 +1,53 @@
 var roots = [];
-function regulaFalsi(f = function(x,a = 1){return  x*x - 2*x + 4}, a, b, dx = 10E-9){
-   let root,
-       fr,
-       i,
-       side=0,
-       fa = f(a),
-       fb = f(b);
-   for (i = 0; i < 1000; i++){
-       root = (fa*b - fb*a) / (fa - fb);
-       if (Math.abs(b-a) < dx*Math.abs(b+a)) break;
-       fr = f(root);
-       if (fr * fb > 0)
-       {
-         b = root; fb = fr;
-         if (side==-1) fa /= 2;
-         side = -1;
-       }
-       else if (fa * fr > 0)
-       {
-         a = root;  fa = fr;
-         if (side==+1) fb /= 2;
-         side = +1;
-       }
-       else
-       {
-         break;
-       } 
+function regulaFalsi(
+  f = function(x, a = 1) {
+    return x * x - 9;
+  },
+  a,
+  b,
+  dx = 10e-9
+) {
+  let root,
+    fr,
+    i,
+    side = 0,
+    fa = f(a),
+    fb = f(b);
+  for (i = 0; i < 1000; i++) {
+    root = (fa * b - fb * a) / (fa - fb);
+    if (Math.abs(f(b)) < dx || Math.abs(f(b)) < dx) {
+      roots.push("no");
+      break;
     }
-    roots.push(root);
+    fr = f(root);
+    if (fr * fb > 0) {
+      console.log("fr * fb > 0", fa, fb, root);
+      b = root;
+      fb = fr;
+      if (side == -1) fa /= 2;
+      side = -1;
+    } else if (fa * fr > 0) {
+      console.log("fr * fa > 0", fa, fb, fr);
+      a = root;
+      fa = fr;
+      if (side == +1) fb /= 2;
+      side = +1;
+    } else {
+      roots.push(root);
+      break;
+    }
+  }
 }
-var xmin = -5, xmax = 5;
-for(let i = xmin; i <= xmax; i += 1/Math.abs(xmax - xmin)){
-  regulaFalsi(function(x,a = 1){return  x*x - 2*x + 4}, i, xmax, 10E-9)
+var xmin = -5,
+  xmax = 5;
+for (let i = xmin; i <= xmax; i += 1 / Math.abs(xmax - xmin)) {
+  regulaFalsi(
+    function(x, a = 1) {
+      return x * x - 2 * x + 4;
+    },
+    i,
+    xmax,
+    10e-9
+  );
 }
 console.log(roots);
