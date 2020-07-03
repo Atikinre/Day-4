8a22f38e1e571152cee848818e8249200d7fd8e4
Glitch (hello-webpage) <none>
2020-07-03 13:45:55 +0000

    🐲⛪️ Checkpoint ./script.js:18290274/247

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
 var roots = [];
 function regulaFalsi(f = function(x,a = 1){return  x*x - 2*x + 4}, xmin = -5, xmax = 5, dx = 10E-9){
-    if (f(xmin) * f(xmax) > 0) { 
+    if (f(xmin) * f(xmax) > 0 || f(xmax) - f(xmin) === NaN) { 
       console.log("нет");
       return false;
-    }
-    if (f(xmin) * f(xmax) < dx) { 
-      console.log(xmin, xmax);
-      
     }
     let c = xmin;    
     for (let i=0; i < 1000; i++) { 
         c = (xmin*f(xmax) - xmax*f(xmin))/ (f(xmax) - f(xmin)); 
-        console.log(c, f(c));
         if (Math.abs(f(c)) <= dx) 
             break; 
         else if (f(c)*f(xmin) < 0) 
@@ -22,6 +17,6 @@ function regulaFalsi(f = function(x,a = 1){return  x*x - 2*x + 4}, xmin = -5, xm
     console.log(c);
     return true;
 }
-var xmin = 0, xmax = 2;
+var xmin = -3, xmax = 2;
 
-regulaFalsi(function(x,a = 1){return  x*x*x*x + 3*x*x*x  - x*x + x -5}, xmin, xmax, 10e-9);
+regulaFalsi(function(x,a = 1){return  x*x - 4*x + 4}, xmin, xmax, 10e-9);
