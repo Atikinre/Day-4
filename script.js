4f3e860952c504cc0446d5f4cffde1d4d4efafcf
Glitch (hello-webpage) <none>
2020-07-05 12:08:28 +0000

    👽🧀 Checkpoint ./script.js:18290274/412

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
-var roots = [];
+var roots = new Set;
 function regulaFalsi(f = function(x,a = 1){return  x*x - 2*x + 4}, xmin = -5, xmax = 5, dx = 10E-9){
-    if (Math.abs(f(xmin) * f(xmax)) > dx || f(xmax) - f(xmin) < dx) { 
-      console.log(f(xmin), f(xmax));
+    if (Math.abs(f(xmin) * f(xmax)) > dx) { 
       return false;
     }
-    let c = xmin;    
+    let c = (xmin + xmax)/2;    
     for (let i=0; i < 1000; i++) { 
         c = (xmin*f(xmax) - xmax*f(xmin))/ (f(xmax) - f(xmin)); 
-        if (Math.abs(f(c)) <= dx) 
-            break; 
-        else if (f(c)*f(xmin) < 0) 
+        if (f(c)*f(xmin) < 0) 
             xmax = c; 
-        else
+        else if (f(c)*f(xmax) < 0) 
             xmin = c; 
+        else break;
     } 
-    console.log(c);
+    roots.add(c);
     return true;
 }
 var xmin = -3, xmax = 3;
-for(var i = xmin; i < xmax; i += 1/Math.abs(xmax + xmin)){
-  console.log(i, i + 1/Math.abs(Math.abs(xmax) + Math.abs(xmin)));
-  regulaFalsi(function(x,a = 1){return  x*x - 4*x + 4}, i, i + 1/Math.abs(Math.abs(xmax) - Math.abs(xmin)), 10e-9);
+for(var i = xmin; i <= xmax; i += 0.1){
+  regulaFalsi(function(x,a = 1){return  x*x - 4}, i, i + 1/Math.abs(Math.abs(xmax) - Math.abs(xmin)), 10e-9);
 }
+console.log(roots);
